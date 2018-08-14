a72f3f69ed5c759acac26c9864f24689feeb7551
Onion Rings <liangbryan2@gmail.com>
2018-08-13 18:05:35 -0700

    add favorite section, not locally stored unfortunately, add some animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,41 @@ var apiKey = "&api_key=dc6zaTOxFJmzC";
 var limit = "&limit=10";
 var offset = 0;
 var previous;
+var favorites = [];
 
+// make gif
+function makeGif(result) {
+    var data = result.data;
+    for (var i = 0; i < data.length; i++) {
+        var imgDiv = $("<div class='image'>");
+        var rating = $("<p>");
+        rating.text("Rating: " + data[i].rating);
+        var image = $("<img class='img' data-state='still'>");
+        image.attr("src", data[i].images.fixed_height_still.url);
+        image.attr("data-still", data[i].images.fixed_height_still.url);
+        image.attr("data-animated", data[i].images.fixed_height.url);
+        var favButton = $("<button class='favButton' data-state='still'>");
+        favButton.attr("title", data[i].title);
+        favButton.attr("src", data[i].images.fixed_height_still.url);
+        favButton.attr("data-still", data[i].images.fixed_height_still.url);
+        favButton.attr("data-animated", data[i].images.fixed_height.url);
+        favButton.text("Favorite");
+        imgDiv.append(rating);
+        imgDiv.append(image);
+        imgDiv.append(favButton);
+        $("#gifSection").prepend(imgDiv);
+    }
+    $(".img").on("click", function () {
+        var image = $(this);
+        playGif(image);
+    });
+    $(".favButton").on("click", function () {
+        var button = $(this);
+        favorite(button);
+    })
+}
+
+// check for offset if needed then call make gif function
 function generateGifs(button) {
     query = button.attr("data-hero");
     if (previous === button.attr("data-hero")) {
@@ -19,23 +53,7 @@ function generateGifs(button) {
             url: baseURL + query + apiKey + limit + "&offset=" + offset,
             method: "GET"
         }).then(function (result) {
-            var data = result.data;
-            for (var i = 0; i < data.length; i++) {
-                var imgDiv = $("<div class='image'>");
-                var rating = $("<p>");
-                rating.text("Rating: " + data[i].rating);
-                var image = $("<img class='img' data-state='still'>");
-                image.attr("src", data[i].images.fixed_height_still.url);
-                image.attr("data-still", data[i].images.fixed_height_still.url);
-                image.attr("data-animated", data[i].images.fixed_height.url);
-                imgDiv.append(rating);
-                imgDiv.append(image);
-                $("#gifSection").prepend(imgDiv);
-            }
-            $(".img").on("click", function () {
-                var image = $(this);
-                playGif(image);
-            });
+            makeGif(result);
         });
     } else {
         offset = 0;
@@ -45,27 +63,12 @@ function generateGifs(button) {
             method: "GET"
         }).then(function (result) {
             console.log(result);
-            var data = result.data;
-            for (var i = 0; i < data.length; i++) {
-                var imgDiv = $("<div class='image'>");
-                var rating = $("<p>");
-                rating.text("Rating: " + data[i].rating);
-                var image = $("<img class='img' data-state='still'>");
-                image.attr("src", data[i].images.fixed_height_still.url);
-                image.attr("data-still", data[i].images.fixed_height_still.url);
-                image.attr("data-animated", data[i].images.fixed_height.url);
-                imgDiv.append(rating);
-                imgDiv.append(image);
-                $("#gifSection").prepend(imgDiv);
-            }
-            $(".img").on("click", function () {
-                var image = $(this);
-                playGif(image);
-            });
+            makeGif(result);
         });
     }
 }
 
+// play gif when click gif
 function playGif(image) {
     var state = image.attr("data-state");
     if (state === "still") {
@@ -78,6 +81,7 @@ function playGif(image) {
 
 }
 
+// Create button when search
 function generateButtons() {
     $("#heroes").empty();
     for (var i = 0; i < heroArray.length; i++) {
@@ -93,6 +97,22 @@ function generateButtons() {
     });
 }
 
+
+function favorite(button) {
+    console.log(favorites);
+    var gif = {
+        title: button.attr("title"),
+        src: button.attr("data-still"),
+        still: button.attr("data-still"),
+        animate: button.attr("data-animated"),
+        state: button.attr("data-state")
+    }
+    favorites.push(gif);
+}
+
+
+
+// Document click
 $(document).ready(function () {
     generateButtons();
     $("#submit").on("click", function (event) {
@@ -102,5 +122,24 @@ $(document).ready(function () {
             heroArray.push(query);
         }
         generateButtons();
+    });
+    $(".favorite").on("click", function() {
+        $("#gifSection").empty();
+        for(var i = 0; i < favorites.length; i++) {
+            var title = $("<p>");
+            var imgDiv = $("<div class='image'>");
+            var image = $("<img class='img' data-state='still'>");
+            title.text(favorites[i].title);
+            image.attr("src", favorites[i].src);
+            image.attr("data-still", favorites[i].still);
+            image.attr("data-animated", favorites[i].animate);
+            imgDiv.append(title);
+            imgDiv.append(image);
+            $("#gifSection").prepend(imgDiv);
+        }
+        $(".img").on("click", function () {
+            var image = $(this);
+            playGif(image);
+        });
     });
 });
